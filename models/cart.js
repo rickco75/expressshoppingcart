56e81968dd588c0679908ebad459242a99e63228
Rick Cohen <rickco@gmail.com>
2019-09-14 20:23:58 -0500

    finished final touches

--- models/cart.js
+++ models/cart.js
@@ -19,7 +19,18 @@ module.exports = function Cart(oldCart) {
         this.items[id].price -= this.items[id].item.price;
         this.totalQty--;
         this.totalPrice -= this.items[id].item.price;
+
+        if (this.items[id].qty <= 0){
+            delete this.items[id];
+        }
     };
+
+    this.removeItem = function(id){
+        this.totalQty-= this.items[id].qty;
+        this.totalPrice -= this.items[id].item.price;
+        delete this.items[id];
+
+    }
 
     this.generateArray = function() {
         var arr = [];
